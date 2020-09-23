6ebd1de44fb645e285f2b4bbb1786040cf82cace
Aquiles Aguiar <aquilessilva@ucl.br>
2020-09-23 16:58:41 +0000

    funcionando.par1

--- main.cs
+++ main.cs
@@ -2,18 +2,21 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+
+    // Informações para cadstro da Consulta
+
     cria_cadastro novo_usuario = new cria_cadastro();
     Console.Write("Digite Seu Nome >> ");
    
     novo_usuario.set_nome(Console.ReadLine());           
     
-    Console.Write(" Insira seu email >>");
+    Console.Write("Insira seu email >>");
     novo_usuario.set_email(Console.ReadLine());          
     
     Console.Write("Insira seu telefone >>");
     novo_usuario.set_telefone(int.Parse(Console.ReadLine()));
     
-    Console.Write(" Insira sua cidade >>");
+    Console.Write("Insira sua cidade >>");
     novo_usuario.set_cidade(Console.ReadLine());
     
     Console.Write("Insira seu Bairro >>");
@@ -22,10 +25,10 @@ class MainClass {
     Console.Write("Insira seu estado >>");
     novo_usuario.set_estado(Console.ReadLine());
     
-    Console.Write("Escolha a especialidade para Atendimento: \n1-Alergia e Imunologia - R$ 100,00\n2-Cirurgia Geral - R$ 69.00\n3-Cardiologia - R$ 78.00\n4-Ginecologia - R$ 95.00\n5-Neurologia - R$ 120.00\n6-Nutrologia - R$ 95.00\n7-Oftalmologia - R$ 95.00\n8-Ortopedia e Traumatologia - R$ 95.00\n9-Otorrinolaringologia - R$ 95.00\n10-Pediatria - R$ 95.00\n11-Psiquiatria - R$ 120.00/n >>");
+    Console.Write("Escolha a especialidade para Atendimento: \n1-Alergia e Imunologia - R$ 100,00\n2-Cirurgia Geral - R$ 69.00\n3-Cardiologia - R$ 78.00\n4-Ginecologia - R$ 95.00\n5-Neurologia - R$ 120.00\n6-Nutrologia - R$ 95.00\n7-Oftalmologia - R$ 95.00\n8-Ortopedia e Traumatologia - R$ 95.00\n9-Otorrinolaringologia - R$ 82.00\n10-Pediatria - R$ 98.00\n11-Psiquiatria - R$ 120.00\n >>");
     novo_usuario.set_especialidade(int.Parse(Console.ReadLine()));
     
-    Console.Write("Possui plano de Saude (Desconto 15%) ? 1 - Sim / 2 - Não >>");
+    Console.Write("Possui plano de Saude ? (Desconto 15%)  \n1 - Sim / 2 - Não >>");
     novo_usuario.set_plano(int.Parse(Console.ReadLine()));
 
     Console.Write("Insira a data da Consulta >>");
@@ -37,8 +40,14 @@ class MainClass {
     Console.Write("Insira a data de Nascimento >>");
     novo_usuario.set_data_nascimento(Console.ReadLine());
 
-    Console.Write(" Insira seu Sexo >> ");
+    Console.Write("Insira seu Sexo >> ");
     novo_usuario.set_sexo(Console.ReadLine());
+
+    //Calculo do Orçamento
+
+    
+
+    Console.WriteLine(orca.calculo_consu(novo_usuario.get_plano(),novo_usuario.get_especialidade()));
     
   }
 }
--- orçamento.cs
+++ orçamento.cs
@@ -3,7 +3,6 @@ using System;
 class orca{
 
   public static string calculo_consu(int especialidade,int plano){
-    double valor_consul;
 
     switch(especialidade){
       
@@ -16,27 +15,77 @@ class orca{
       
       case 2:
         if (plano == 1){
-        return Console.Write($"O Valor da Consulta com desconto >> {69-(69*0.15)}");
-        
+          return $"O Valor da Consulta com desconto >> {69-(69*0.15)}";
         }
         else
-          return Console.Write($"O Valor da Consulta Integral >> {69}");
+          return $"O Valor da Consulta Integral >> {69}";
 
       case 3:
           if (plano == 1){
-          return Console.Write($"O Valor da Consulta com desconto >> {78-(78*0.15)}");
-          
+            return $"O Valor da Consulta com desconto >> {78-(78*0.15)}";
           }
           else
-            return Console.Write($"O Valor da Consulta Integral >> {78}");
+            return $"O Valor da Consulta Integral >> {78}";
 
       case 4:
           if (plano == 1){
-          return Console.Write($"O Valor da Consulta com desconto >> {95-(95*0.15)}");
-          
+           return $"O Valor da Consulta com desconto >> {95-(95*0.15)}";
           }
           else
-            return Console.Write($"O Valor da Consulta Integral >> {95}");
+            return $"O Valor da Consulta Integral >> {95}";
+      
+      case 5:
+          if (plano == 1){
+           return $"O Valor da Consulta com desconto >> {120-(120*0.15)}";
+          }
+          else
+            return $"O Valor da Consulta Integral >> {120}";
+      
+      case 6:
+          if (plano == 1){
+           return $"O Valor da Consulta com desconto >> {95-(95*0.15)}";
+          }
+          else
+            return $"O Valor da Consulta Integral >> {95}";
+      
+      case 7:
+          if (plano == 1){
+           return $"O Valor da Consulta com desconto >> {95-(95*0.15)}";
+          }
+          else
+            return $"O Valor da Consulta Integral >> {95}";
+      
+      case 8:
+          if (plano == 1){
+           return $"O Valor da Consulta com desconto >> {95-(95*0.15)}";
+          }
+          else
+            return $"O Valor da Consulta Integral >> {95}";
+      
+      case 9:
+          if (plano == 1){
+           return $"O Valor da Consulta com desconto >> {82-(82*0.15)}";
+          }
+          else
+            return $"O Valor da Consulta Integral >> {82}";
+      
+      case 10:
+          if (plano == 1){
+           return $"O Valor da Consulta com desconto >> {98-(98*0.15)}";
+          }
+          else
+            return $"O Valor da Consulta Integral >> {98}";
+      
+      case 11:
+          if (plano == 1){
+           return $"O Valor da Consulta com desconto >> {120-(120*0.15)}";
+          }
+          else
+            return $"O Valor da Consulta Integral >> {120}";
+      
+      
+      default:
+        return "Um Valor Indesejado foi inserido";
       
       
       
